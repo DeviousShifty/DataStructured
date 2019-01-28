8a6762818d27340aaa99c5b924da74a3a87c93e0
Ethan M <guitarworthy365@gmail.com>
2019-01-28 09:03:57 -0700

    Created Controllers, runner, imported node pngs

---
+++ DataStructured/Controller/Controller.cpp
@@ -0,0 +1,14 @@
+//
+//  Controller.cpp
+//  DataStructured
+//
+//  Created by Mumford, Ethan on 1/28/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+void Controller :: start()
+{
+    cout << "Welcome to the Data Structures Array"
+}
