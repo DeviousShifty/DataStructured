f9bf6f226df4de5eaab0a3defe1d0e13058f4149
Ethan M <guitarworthy365@gmail.com>
2019-02-07 09:28:57 -0700

    Finished Linear node class.

---
+++ DataStructured/Linear/Array.hpp
@@ -0,0 +1,13 @@
+//
+//  Array.hpp
+//  DataStructured
+//
+//  Created by Mumford, Ethan on 2/7/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef Array_h
+#define Array_h
+
+
+#endif /* Array_h */
